486d8ac99094aa7bee1bd5c2474c6b547bd517a8
Ville Nuutinen <hunttis@me.com>
2018-10-14 15:31:18 +0300

    Added quick categorization of storecredit events.

--- src/components/AdminTools/StoreCredit/StoreCreditRow.js
+++ src/components/AdminTools/StoreCredit/StoreCreditRow.js
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 
-export const StoreCreditRow = ({ dataId, data, entryMadeBy, isAdmin }) => (
-  <tr>
+export const StoreCreditRow = ({ dataId, data, entryMadeBy, isAdmin, updateCategory }) => (
+  <tr className={`${data.category === 'green' && 'has-text-success'} ${data.category === 'red' && 'has-text-danger'} ${data.category === 'yellow' && 'has-text-warning'} ${data.category === 'blue' && 'has-text-info'}`}>
     {isAdmin &&
       <td className="is-size-7">{dataId}</td>
     }
@@ -11,6 +11,23 @@ export const StoreCreditRow = ({ dataId, data, entryMadeBy, isAdmin }) => (
     <td>{entryMadeBy}</td>
     <td>{data.note}</td>
     <td className={`${data.value > 0 ? 'has-text-success' : 'has-text-danger'}`}>{data.value} €</td>
+    <td>
+      <span onClick={() => updateCategory(dataId, '')}>
+        <i className="fas fa-circle has-text-white" />
+      </span>
+      <span onClick={() => updateCategory(dataId, 'green')}>
+        <i className="fas fa-circle has-text-success" />
+      </span>
+      <span onClick={() => updateCategory(dataId, 'red')}>
+        <i className="fas fa-circle has-text-danger" />
+      </span>
+      <span onClick={() => updateCategory(dataId, 'yellow')}>
+        <i className="fas fa-circle has-text-warning" />
+      </span>
+      <span onClick={() => updateCategory(dataId, 'blue')}>
+        <i className="fas fa-circle has-text-info" />
+      </span>
+    </td>
   </tr>
 );
 
@@ -19,4 +36,5 @@ StoreCreditRow.propTypes = {
   data: PropTypes.object,
   entryMadeBy: PropTypes.string,
   isAdmin: PropTypes.bool,
+  updateCategory: PropTypes.func,
 };
