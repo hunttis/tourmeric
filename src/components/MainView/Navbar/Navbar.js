acf4761d450f71304ffce62af07b17d7b90b04f5
Ville Nuutinen <hunttis@me.com>
2018-11-03 08:31:52 +0200

    Show pagetitle in the navbar if there is no active logo

--- src/components/MainView/Navbar/Navbar.js
+++ src/components/MainView/Navbar/Navbar.js
@@ -54,7 +54,12 @@ export default class Navbar extends Component {
         <div className="navbar is-white" role="navigation" aria-label="dropdown navigation">
           <div className="navbar-brand">
             <div className="navbar-item is-hidden-desktop">
-              <img src={settings.activeLogo} alt="" />
+              {settings.activeLogo &&
+                <img src={settings.activeLogo} alt="" />
+              }
+              {!settings.activeLogo &&
+                <div>{settings.pageTitle}</div>
+              }
             </div>
 
             <div role="button" className={`navbar-burger ${this.state.burgerOpen && 'is-active'}`} tabIndex={0} onClick={() => { this.toggleBurger(); }}>
